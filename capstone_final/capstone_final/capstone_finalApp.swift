958184553798d2b792dc6dd39d57ad82bc2a4c67
RACHEL056 <135407041+RACHEL056@users.noreply.github.com>
2023-07-22 16:16:00 +0900

    Add files via upload

---
+++ capstone_final/capstone_final/capstone_finalApp.swift
@@ -0,0 +1,17 @@
+//
+//  capstone_finalApp.swift
+//  capstone_final
+//
+//  Created by 박민정 on 2023/07/22.
+//
+
+import SwiftUI
+
+@main
+struct capstone_finalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
